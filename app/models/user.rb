bde3a956d98bef55df387a9135d84ec8c81c7f5e
robinbshrestha <robinbshrestha@gmail.com>
2019-05-28 17:06:39 -0400

    Prepare for initial Heroku push

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+    attr_reader :password
+
+    validates :email, :password_digest, :session_token presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(user_params)
+        user = User.find_by(email: user_params[:email])
+        user && user.is_password?(user_params[:password]) ? user : nil
+    end
+
+    def ensure_session_token
+        self.session_token = SecureRandom::urlsafe_base64
+    end
+
+    def is_password?(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token
+        update!(session_token: SecureRandom::urlsafe_base64)
+        session_token
+    end
+
+end
