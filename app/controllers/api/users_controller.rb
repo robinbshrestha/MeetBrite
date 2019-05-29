895644c4392c75f0e3ff4a0b4596cda5e99b4b05
robinbshrestha <robinbshrestha@gmail.com>
2019-05-29 11:23:41 -0400

    add session actions and reducer

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ class Api::UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             login(@user)
-            # render :showpage
+            render 'api/users/show'
         else
             render json: @user.errors.full_messages
         end
---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,47 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+export const receiveCurrentUser = currentUser => {
+    return ({
+        type: RECEIVE_CURRENT_USER,
+        currentUser
+    });
+};
+
+export const logoutCurrentUser = () => {
+    return ({
+        type: LOGOUT_CURRENT_USER,
+    });
+};
+
+export const receiveErrors = errors => {
+    return {
+        type: RECEIVE_SESSION_ERRORS,
+        errors
+    };
+};
+
+export const signup = user => dispatch => {
+    return (
+        SessionAPIUtil.signup(user).then((user) => dispatch(receiveCurrentUser(user)),
+        errors => dispatch(receiveErrors(errors.responseJSON)))
+    );
+};
+
+export const login = user => dispatch => {
+    return (
+        SessionAPIUtil.login(user).then((user) => dispatch(receiveCurrentUser(user)),
+        errors => dispatch(receiveErrors(errors.responseJSON)))
+    );
+};
+
+export const logout = () => dispatch => {
+    return (
+        SessionAPIUtil.logout().then(() => dispatch(logoutCurrentUser()))
+    );
+};
+
+
