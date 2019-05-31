476c08b66fdad4fefd5121d7179e87d81b3c94e7
robinbshrestha <robinbshrestha@gmail.com>
2019-05-31 16:57:23 -0400

    adjust video styling

--- frontend/components/nav/navbar.jsx
+++ frontend/components/nav/navbar.jsx
@@ -16,22 +16,18 @@ export default () => (
             </div> 
             
 
-            <div class="video-section">
-
-            <video autoPlay loop muted>
+            <div class="video-container">
+                <video id="video" autoPlay loop muted>
                     <source src="https://www.meetup.com/mu_static/en-US/video.dddafbfe.mp4" type="video/mp4" />
-            </video>
-
-
+                  </video>
+                    <div className="overlay">
+                        <h2>The real world is calling</h2>
+                        <h3>Join a local group to meet people, try something new, or do more of what you love.</h3>
+                        <span className="center-button">
+                            <h4>Join Meetup</h4>
+                        </span>
+                    </div>
             </div>
-
-            <div className="center-text"> 
-                <h2>The real world is calling</h2>
-                <h3>Join a local group to meet people, try something new, or do more of what you love.</h3>
-            </div>
-            <span className="center-button"> 
-                <h4>Join Meetup</h4>
-            </span>
         </div>
     </div>
 )
