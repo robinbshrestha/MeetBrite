4c1c2104f81dc71f5445dc3170fae8134a3709f7
robinbshrestha <robinbshrestha@gmail.com>
2019-05-31 15:49:50 -0400

    add video splash

--- frontend/components/nav/navbar.jsx
+++ frontend/components/nav/navbar.jsx
@@ -13,8 +13,18 @@ export default () => (
                     <Link to="/login" className="login">Log in</Link>
                     <Link to="/register">Sign up</Link>
                 </div>
-            </div>
+            </div> 
             
+
+            <div class="video-section">
+
+            <video autoPlay loop muted>
+                    <source src="https://www.meetup.com/mu_static/en-US/video.dddafbfe.mp4" type="video/mp4" />
+            </video>
+
+
+            </div>
+
             <div className="center-text"> 
                 <h2>The real world is calling</h2>
                 <h3>Join a local group to meet people, try something new, or do more of what you love.</h3>
