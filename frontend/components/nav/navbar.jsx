06405f5942a4e962fc8a2af03ad8583083ae592f
robinbshrestha <robinbshrestha@gmail.com>
2019-05-31 11:19:49 -0400

    add navbar styling

--- frontend/components/nav/navbar.jsx
+++ frontend/components/nav/navbar.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default () => (
-    <div className="nav-bar">
-   
-        <Link to="/" className="logo">Meetup</Link>
-        <h2>The real world is calling</h2>
-        <Link to="./login">Log in</Link>
+    <div>
         <div>
-            <Link to="./register">Sign up</Link>
+            <div className="nav-bar">
+                <div className="nav-bar-left">
+                    <Link to="/" className="logo">Meetup</Link>
+                </div>    
+                <div className="nav-bar-right">
+                    <Link to="./login">Log in</Link>
+                    <Link to="./register">Sign up</Link>
+                </div>
+            </div>
+            
+            <div className="center-text"> 
+                <h2>The real world is calling</h2>
+                <h3>Join a local group to meet people, try something new, or do more of what you love.</h3>
+            </div>
+            <span className="center-button"> 
+                <h4>Join Meetup</h4>
+            </span>
         </div>
     </div>
 )
