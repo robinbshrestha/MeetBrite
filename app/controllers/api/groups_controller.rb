695b916d1215dc3fb7dc9ee69a7c694f08b8251a
robinbshrestha <robinbshrestha@gmail.com>
2019-06-05 17:05:10 -0400

    adjust seed

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -44,6 +44,6 @@ class Api::GroupsController < ApplicationController
 
     private
     def group_params
-        params.require(:group).permit(:title, :description, :location_id, :organizer_id, :img)
+        params.require(:group).permit(:title, :description, :location_id, :organizer_id, :photo)
     end
 end
--- frontend/components/groups/group_index_item.jsx
+++ frontend/components/groups/group_index_item.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const GroupIndexItem = props => {
+    let photo;
+    if (props.group.photo) {
+        photo = <img src={props.group.photo} />;
+    }
 
     return (
         <div>
         <Link to={`/groups/${props.group.id}`}>
             <h3>{props.group.description}</h3>
         </Link>
+        {photo}
         </div>
     )
 }
