7ae09bd08b6cc6c92e78961a5e1197df0ae0fc4c
robinbshrestha <robinbshrestha@gmail.com>
2019-06-06 10:36:25 -0400

    photo grid

--- frontend/components/groups/group_index_item.jsx
+++ frontend/components/groups/group_index_item.jsx
@@ -4,19 +4,16 @@ const GroupIndexItem = props => {
     let photo;
     if (props.group.photo) {
         photo = <img src={props.group.photo} />;
+    } else { photo = <img src="https://meetbrite-seeds.s3.amazonaws.com/photo4.jpg" />
+
     }
 
     return (
         <div>
-        <Link to={`/groups/${props.group.id}`}>
+        <Link className="grid-link-text" to={`/groups/${props.group.id}`}>
             <h3 className="grid-text">{props.group.title}</h3>
         </Link>
             <div className='grid-group'>{photo}</div>
-            {/* <div className='grid-group'><img src="https://meetbrite-seeds.s3.amazonaws.com/photo3.png"/></div>
-            <div className='grid-group'><img src="https://meetbrite-seeds.s3.amazonaws.com/photo4.jpg"/></div>
-            <div className='grid-group'><img src="https://meetbrite-seeds.s3.amazonaws.com/photo5.png"/></div>
-            <div className='grid-group'><img src="https://meetbrite-seeds.s3.amazonaws.com/photo6.jpeg"/></div>
-            <div className='grid-group'><img src="https://meetbrite-seeds.s3.amazonaws.com/photo7.jpeg"/></div> */}
         </div>
         
     )
