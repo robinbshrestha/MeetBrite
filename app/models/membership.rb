5fe0764d76ee82eabbfbe43627dc4a8ab4b23c2c
robinbshrestha <robinbshrestha@gmail.com>
2019-06-10 15:57:30 -0400

    debug heroku error

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,18 +1,14 @@
 class Membership < ApplicationRecord
-    # belongs_to :user,
-    # foreign_key: :user_id,
-    # class_name: 'User'
+    belongs_to :user,
+    foreign_key: :user_id,
+    class_name: 'User'
 
-    # belongs_to :group,
-    # foreign_key: :group_id,
-    # class_name: 'Group'
+    belongs_to :group,
+    foreign_key: :group_id,
+    class_name: 'Group'
 
-    # has_many :memberships,
-    # through: :user_memberships,
-    # source: 'User'
+    has_many :memberships,
+    through: :user_memberships,
+    source: 'User'
 
-  validates :user_id, :group_id, presence: true
-
-  belongs_to :user 
-  belongs_to :group
 end
